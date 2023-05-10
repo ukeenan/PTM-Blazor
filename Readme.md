2f95d6182e30bbb5fcfb3143b78227a11eeef6fe
ukeenan <hadinug@live.com>
2023-05-10 10:59:47 +0700

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,65 @@
+﻿# Integratasi SSO IdAMAn dengan NET Core Balzor Assembly untuk WEB dan API
+
+## Sources
+Go to ->
+
+
+
+## Registrasi Aplikasi 
+Daftarkan aplikasi yang akan menggunakan IdAMan seperti berikut:
+
+1. Buka aplikasi IdAMan QA  <https://identity.qa.idaman.pertamina.com/> 
+1. Masuk ke menu Applications -> new Applications
+
+Isikan,
+Name: SPA - Blazor Assembly Example
+redirect URL: <https://localhost:5001/authentication/login-callback>
+Type: Single Page / Mobile
+Company: Pertamina Persero
+
+Seperti pada gambar berikut:
+
+Kemudian submit, maka pada list aplikasi akan muncul daftar aplikasi Balzor Asembly Example, klik dan lihat detail nya.
+
+1. Define scope untuk API
+       - Enable API
+
+       - Add new scope
+## WEB Projects
+
+1. Setup configurations di wwwroot/appsettings.json
+
+
+1. Pada project API set up Authorization URL dan Scope nya Program.cs
+
+1. Run project donet run -> running project in <https://localhost:5001> 
+
+
+
+
+
+Klik button login -> page akan meredirect ke halaman Login IdAMan
+
+
+
+
+
+
+
+
+
+
+
+
+1. Show claims -> [BlazorClient](https://localhost:5001/claims)
+
+
+
+
+## API Project
+1. Set configutation in Startup.cs
+
+
+1. Run project -> dotnet run -> [](https://localhost:5002/whe) https://localhost:5002/WeatherForecast
+
+##
---
+++ WeatherApi/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WeatherApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://login.qa.idaman.pertamina.com";
+                    options.Audience = "api://5fb81914-071a-44c4-857f-a264f5faa7ca";
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        NameClaimType = "name"
+                    };
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(config => 
+            {
+                config.AllowAnyOrigin();
+                config.AllowAnyMethod();
+                config.AllowAnyHeader();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
